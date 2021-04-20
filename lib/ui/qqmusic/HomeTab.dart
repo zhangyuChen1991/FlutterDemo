3c522e8a6a0510af68e425f00c826c6a44af08a3
zhangyuChen1991 <chenzhangyu1003@foxmail.com>
2021-04-20 19:45:43 +0800

    HomeTab 页面结构

--- lib/ui/qqmusic/HomeTab.dart
+++ lib/ui/qqmusic/HomeTab.dart
@@ -1,24 +1,56 @@
+import 'package:first_flutter_app/ui/qqmusic/HomeTabRadioPage.dart';
+import 'package:first_flutter_app/ui/qqmusic/HomeTabRecommendPage.dart';
 import 'package:flutter/material.dart';
 
-/// 创建时间：2021/4/20 
+/// 创建时间：2021/4/20
 /// 作者：4cv748wpdb
 /// 描述：
 
-class HomeTab extends StatefulWidget{
+class HomeTab extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return HomeTabState();
   }
 }
 
-class HomeTabState extends State{
+class HomeTabState extends State {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[200],
-      width: double.infinity,
-      height: double.infinity,
+    return Column(
+      children: [
+        TitleBar(),
+        Expanded(
+            child: PageView(
+          scrollDirection: Axis.horizontal,
+          children: [HomeTabRecommendPage(), HomeTabRadioPage()],
+        ))
+      ],
     );
   }
+}
 
+class TitleBar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TitleBarState();
+  }
+}
+
+class TitleBarState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [getTitleBarText("推荐"), getTitleBarText("电台")],
+        ),
+      ],
+    );
+  }
+}
+
+getTitleBarText(String title) {
+  return Container(
+      height: 50,
+      child: (Text(title, style: new TextStyle(color: Color(0xff263138)))));
 }
---
+++ lib/ui/qqmusic/HomeTabRecommendPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// 创建时间：2021/4/20 
+/// 作者：4cv748wpdb
+/// 描述：
+
+class HomeTabRecommendPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return HomeTabRecommendPageState();
+  }
+}
+
+class HomeTabRecommendPageState extends State{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[300],
+      width: double.infinity,
+      height: double.infinity,
+    );
+  }
+
+}
