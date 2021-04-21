63332c15674221ffe536451914d6583dc0806785
zhangyuChen1991 <chenzhangyu1003@foxmail.com>
2021-04-21 19:37:37 +0800

    主页HomeTab顶部PagerView和指示器

--- lib/ui/qqmusic/HomeTab.dart
+++ lib/ui/qqmusic/HomeTab.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:first_flutter_app/ui/qqmusic/HomeTabRadioPage.dart';
 import 'package:first_flutter_app/ui/qqmusic/HomeTabRecommendPage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// 创建时间：2021/4/20
@@ -14,15 +17,33 @@ class HomeTab extends StatefulWidget {
 }
 
 class HomeTabState extends State {
+  TitleBar titleBar = TitleBar();
+
+
   @override
   Widget build(BuildContext context) {
+
+    PageController mPageController = new PageController();
+    mPageController.addListener(() {
+
+      var mPageOffset = mPageController.offset;//相对于整个PageView滚动控件，位置偏移量(滚动的距离)
+      var mPageIndex = mPageController.page;//从第一个tab滑到第二个tab的过程中，mPageIndex会从0逐渐变到1
+      log("mPageOffset: "+mPageOffset.toString()+", mPageIndex: "+mPageIndex.toString());
+
+      //计算滑动指示器的左边距，直接套用这个公式就可以了: 初始左边距加上tab间距乘以mPageController.page
+      var leftMargin = 10+ mPageIndex* 50;
+      titleBar.titleBarState.setState(() {
+        _mIndicatorLeftMargin = leftMargin;
+      });
+    });
     return Column(
       children: [
-        TitleBar(),
+        titleBar,
         Expanded(
-            child: PageView(
-          scrollDirection: Axis.horizontal,
-          children: [HomeTabRecommendPage(), HomeTabRadioPage()],
+          child: PageView(
+            controller: mPageController,
+            scrollDirection: Axis.horizontal,
+            children: [HomeTabRecommendPage(), HomeTabRadioPage()],
         ))
       ],
     );
@@ -30,20 +51,39 @@ class HomeTabState extends State {
 }
 
 class TitleBar extends StatefulWidget {
+  TitleBarState titleBarState = TitleBarState();
   @override
   State<StatefulWidget> createState() {
-    return TitleBarState();
+    return titleBarState;
   }
 }
 
+double _mIndicatorLeftMargin = 10;
 class TitleBarState extends State {
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Container(
+          height: 30,
+        ),
         Row(
           children: [getTitleBarText("推荐"), getTitleBarText("电台")],
         ),
+        Container(
+          margin: EdgeInsets.fromLTRB(_mIndicatorLeftMargin, 10, 0, 0),
+          height: 5,
+          width: 30,
+          decoration: new BoxDecoration(
+            border: new Border.all(color: Color(0xFF22D59C), width: 0.5), // 边色与边宽度
+            color: Color(0xFF22D59C), // 底色
+            //        shape: BoxShape.circle, // 圆形，使用圆形时不可以使用borderRadius
+            shape: BoxShape.rectangle, // 默认值也是矩形
+            borderRadius: new BorderRadius.circular((5.0)), // 圆角度
+          ),
+        )
       ],
     );
   }
@@ -51,6 +91,9 @@ class TitleBarState extends State {
 
 getTitleBarText(String title) {
   return Container(
-      height: 50,
-      child: (Text(title, style: new TextStyle(color: Color(0xff263138)))));
+      height: 40,
+      width: 50,
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
+      child: (Text(title, style: new TextStyle(fontSize:16,fontStyle: FontStyle.normal,color: Color(0xFF263138)))));
 }
